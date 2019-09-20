1373fd6ab11387fb954decf2deeb5fa59bcaab5a
Harum Helmy <harumendhah@gmail.com>
2019-09-20 09:04:59 -0400

    moved some redux folders around

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from './components/Login'
 import Navbar from './components/Navbar'
 import Home from './containers/Home'
 import FriendsContainer from './containers/FriendsContainer'
-import store from './store/index' // redux store 
+import store from './redux/store/index' // redux store 
 
 class App extends React.Component {
   
