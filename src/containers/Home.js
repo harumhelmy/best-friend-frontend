486383ecfeeed09b7b64d6272191f1a7bd7e58b6
Harum Helmy <harumendhah@gmail.com>
2019-09-24 10:22:42 -0500

    switching to semantic-ui for now

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import NewFriendForm from '../modals/NewFriendForm'
 
 const Home = (props) => {
   
     return(
       <div>
         <h3>welcome to bestFriend, {props.currentUser.username}! this your home page. </h3>
+        <NewFriendForm />
       </div>
     )
 }
--- src/modals/NewFriendForm.js
+++ src/modals/NewFriendForm.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {Fragment} from 'react'
+import { Button, Form } from 'semantic-ui-react'
 
 const NewFriendForm = () => {
-  
+  return (
+  <Form onSubmit={()=>console.log('hi')}>
+    <h3>add a new friend</h3>
+      <Form.Field>
+        <label>name</label>
+        <input className="input" type="text" placeholder="name"/>
+      </Form.Field>
+      <Form.Field>
+        <label>pronouns</label>
+        <input className="input" type="text" placeholder="pronouns"/>
+      </Form.Field>
+      <Form.Field>
+        <label>appreciation</label>
+        <textarea className="textarea" type="text" placeholder="what do you appreciate about this friend?"/>
+      </Form.Field>
+      <Button type='submit' className='ui button'>Submit</Button>
+  </Form>
+  )
+}
+
+const mapDispatchToProps = () => {
+  return {
+    
+  }
 }
 
 export default NewFriendForm 
