0152f0d2c08628110808786ded78fa15ab47cc84
Harum Helmy <harumendhah@gmail.com>
2019-10-16 11:51:16 -0400

    added a column to get the friends list page a bit more centered

--- src/containers/FriendsContainer.js
+++ src/containers/FriendsContainer.js
@@ -19,58 +19,55 @@ class FriendsContainer extends React.Component{
     <Fragment>
       { this.state.isLoading ? null : 
         this.props.friends.length === 0 ?
-        <div className="columns is-mobile">
-        <div className="column is-three-fifths is-offset-one-fifth">
-            <h2>start adding friends! &hearts; </h2>
-            <NewFriendForm />
-          </div>
-        </div>
-        :
-        <Fragment>
-        <div className='container'>
-          <div className="hero">
-            <div className="hero-body">
-              <div className="container">
-                <h1 className="title">
-                  here's everyone &hearts;
-                </h1>
-                <small><Link to='/newfriend'>go here to add a new friend, if you wanna</Link></small>
-              </div>
+          <div className="columns is-mobile">
+            <div className="column is-three-fifths is-offset-one-fifth">
+                <h2>start adding friends! &hearts; </h2>
+                <NewFriendForm />
             </div>
           </div>
-        </div>
+          :
+          <Fragment>
+            
+            <div className='container'>
+              <div className="hero">
+                <div className="hero-body">
+                  <div className="container">
+                    <h1 className="title">
+                      here's everyone &hearts;
+                    </h1>
+                    <small><Link to='/newfriend'>go here to add a new friend, if you wanna</Link></small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div className='columns'>
+              <div className='column is-1'></div>
+              <div className='columns is-multiline'>
+                {
+                  this.props.friends.length < 2 ?
 
-      <div className='container' 
-        style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}> 
-
-        <div className='columns is-multiline'>
-          {
-            this.props.friends.length < 2 ?
-
-            this.props.friends.map( friend => {
-              return (
-              <div className='column is-8'>
-              <FriendCard 
-                  key={`friend-${friend.id}`} 
-                  friend={friend} /> 
-              </div> ) 
-            })
-              :
-             this.props.friends.map( friend => {
-              return (
-              <div className='column is-3'>
-              <FriendCard 
-                  key={`friend-${friend.id}`} 
-                  friend={friend} /> 
-              </div> ) 
-            })
-             
-          }
-        </div>
-      </div>
-
-    
-      </Fragment>
+                  this.props.friends.map( friend => {
+                    return (
+                    <div className='column is-8'>
+                    <FriendCard 
+                        key={`friend-${friend.id}`} 
+                        friend={friend} /> 
+                    </div> ) 
+                  })
+                    :
+                  this.props.friends.map( friend => {
+                    return (
+                    <div className='column is-3'>
+                    <FriendCard 
+                        key={`friend-${friend.id}`} 
+                        friend={friend} /> 
+                    </div> ) 
+                  })   
+                }
+              </div>
+            </div>
+          </Fragment>
       }}
     </Fragment>
     )
