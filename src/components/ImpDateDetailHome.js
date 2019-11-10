203e35c779c89c4030a1783645c15a5802503acd
Harum Helmy <harumendhah@gmail.com>
2019-11-10 17:25:12 -0500

    added link to friend showpage in important date detail

--- src/components/ImpDateDetailHome.js
+++ src/components/ImpDateDetailHome.js
@@ -1,15 +1,18 @@
 import React, {Fragment} from 'react'
 import moment from 'moment'
+import { Link } from 'react-router-dom'
 
 class ImpDateDetailHome extends React.Component { 
 
   render(){
 
   const { date, title } = this.props.date
+  const { id, name } = this.props.date.friend
 
     return (
       <Fragment>
-        <li><strong>{moment(date).format('LL')}</strong> || <strong>{moment(date).fromNow()}</strong></li>
+        <li><strong>{moment(date).format('LL')}</strong> || <strong>{moment(date).fromNow()} </strong> 
+          with <Link to={`/friends/${id}`}>{name}</Link></li>
         <p>{title}</p>
       </Fragment>
     )
